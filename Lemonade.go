e9096dcd9727a8a69ae8490e19565069687c8f2e
annikoff <annikoff@ya.ru>
2014-04-15 23:35:34 +0400

    Подключил html-парсер

--- Lemonade.go
+++ Lemonade.go
@@ -2,66 +2,76 @@ package main
 
 import (
     "github.com/conformal/gotk3/gtk"
+    "github.com/opesun/goquery"
     "log"
+    "fmt"
 )
 
+// Setup the Window.
+func setupWindow() *gtk.Window {
+    w, _ := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+    w.Connect("destroy", gtk.MainQuit)
+    w.SetDefaultSize(500, 300)
+    w.SetPosition(gtk.WIN_POS_CENTER)
+    w.SetTitle("Lemonade - web-scanner")
+
+    return w
+}
+
+func parsePage(html string) {
+    x, err := goquery.ParseString(html)
+    if err != nil {
+        log.Fatal("Parse error:", err)
+        return
+    }
+
+    links := x.Find("a")
+    for _, link := range links {
+        attrs := link.Node.Attr
+        for _, attr := range attrs {
+            fmt.Print(attr.Val)
+        }
+    }
+}
+
 func main() {
+
     gtk.Init(nil)
 
-    win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-    if err != nil {
-        log.Fatal("Unable to create window:", err)
-    }
-    win.SetTitle("Lemonade - web-scanner")
-    win.Connect("destroy", func() {
-        gtk.MainQuit()
-    })
+    win := setupWindow()
 
-    grid, err := gtk.GridNew()
+    topGrid, err := gtk.GridNew()
     if err != nil {
         log.Fatal("Unable to create grid:", err)
     }
-    grid.SetOrientation(gtk.ORIENTATION_VERTICAL)
+    topGrid.SetOrientation(gtk.ORIENTATION_HORIZONTAL)
 
+    urlLabel, err := gtk.LabelNew(" URL: ")
+    if err != nil {
+        log.Fatal("Unable to create label:", err)
+    }
 
-    entry, err := gtk.EntryNew()
+    urlEntry, err := gtk.EntryNew()
     if err != nil {
         log.Fatal("Unable to create entry:", err)
     }
 
-    spnBtn, err := gtk.SpinButtonNewWithRange(0.0, 1.0, 0.001)
+    startButton, err := gtk.ButtonNewWithLabel("Start")
     if err != nil {
-        log.Fatal("Unable to create spin button:", err)
+        log.Fatal("Unable to create entry:", err)
     }
 
-    nb, err := gtk.NotebookNew()
-    if err != nil {
-        log.Fatal("Unable to create notebook:", err)
-    }
-grid.Add(entry)
-//    grid.Add(btn)
-    grid.Add(lab)
-    
-    grid.Add(spnBtn)
+    startButton.Connect("clicked", func() {
+        parsePage("<html><a hre='/test1'>asd</a> <a hre='/test2'>asd</a></html>")
+        return
+    })
 
-    grid.Attach(nb, 1, 1, 1, 2)
-    nb.SetHExpand(true)
-    nb.SetVExpand(true)
+    topGrid.Add(urlLabel)
+    topGrid.Add(urlEntry)
+    topGrid.Add(startButton)
+    win.Add(topGrid)
 
-    // Add a child widget and tab label to the notebook so it renders.
-   /* nbChild, err := gtk.LabelNew("Notebook content")
-    if err != nil {
-        log.Fatal("Unable to create button:", err)
-    }
-    nbTab, err := gtk.LabelNew("Tab label")
-    if err != nil {
-        log.Fatal("Unable to create label:", err)
-    }*/
-  //  nb.AppendPage(nbChild, nbTab)
-
-    // Add the grid to the window, and show all widgets.
-    win.Add(grid)
     win.ShowAll()
 
     gtk.Main()
-} 
+}
